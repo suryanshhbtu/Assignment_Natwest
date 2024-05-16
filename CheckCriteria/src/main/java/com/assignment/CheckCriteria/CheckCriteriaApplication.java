3ba7db15c417a0576757d337832fc044fc46abee
Suryansh Srivastava <suryanshhbtu@gmail.com>
2024-05-16 21:03:43 +0530

    initial commit

---
+++ CheckCriteria/src/main/java/com/assignment/CheckCriteria/CheckCriteriaApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.CheckCriteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckCriteriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckCriteriaApplication.class, args);
+	}
+
+}
---
+++ CheckCriteria/src/test/java/com/assignment/CheckCriteria/CheckCriteriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.CheckCriteria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckCriteriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
